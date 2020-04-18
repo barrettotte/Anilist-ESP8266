1b76d326eb0b31d88f886d9030a63252c31f480c
Barrett <barrettotte@gmail.com>
2020-04-18 01:11:00 -0400

    successful POST request

--- src/main.cpp
+++ src/main.cpp
@@ -1,109 +1,136 @@
 #include <Arduino.h>
 
+/* JSON support */
+#include <ArduinoJson.h>
+
 /* OLED imports */
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Wire.h>
 
-/* WiFi and HTTP requests */
+/* WiFi and HTTPS requests */
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
-#include <WiFiClient.h>
+#include <WiFiClientSecureBearSSL.h>
 
-#include "config.h" // WiFi credentials, Anilist user target
+/* Configuration and secrets */
+#include "config.h" 
 
 // OLED dimensions
 #define SCREEN_WIDTH  128
 #define SCREEN_HEIGHT 64
 
 
-void initSerial();
 void initWifi();
 void initDisplay();
+void clearDisplay();
+void dualPrint(const String line);
+void dualPrint(const String line, bool clr);
+
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 
 void setup(){
-  initSerial();
-  initWifi();
+  Serial.begin(115200);
+  Serial.println("\n* * * START * * *");
   initDisplay();
-  delay(100);
+  initWifi();
+  delay(1500);
+
+  if(WiFi.status() == WL_CONNECTED){
+    int status = 0;
+    char reqStr[256];
+
+    StaticJsonDocument<200> reqJson;
+    HTTPClient https;
+    std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
+    client->setInsecure(); // we're not sending anything sensitive, all is good
+
+    String host = "https://graphql.anilist.co";
+    //dualPrint("[HTTPS] Sending POST request...", true);
+
+    if(https.begin(*client, host)){
+      https.addHeader("Content-Type", "application/json");
+      reqJson["query"] = "{User(search:\"barrettotte\"){id name statistics{anime{meanScore episodesWatched minutesWatched}}}}";
+      serializeJson(reqJson, reqStr);
+
+      Serial.printf("[HTTPS] REQ: %s\n", reqStr);
+      status = https.POST(reqStr);
+        
+      if(status > 0){
+        const String& resp = https.getString();
+        //dualPrint("\n\n[HTTPS] POST " + String(status) + "\n");
+        Serial.printf("[HTTPS] POST %d\n", status);
+
+        if(status == HTTP_CODE_OK){  
+          Serial.printf("[HTTPS] RESP: %s\n", resp.c_str());
+
+        } else if(status >= 400){
+          Serial.printf("[HTTPS] ERR: %s\n", resp.c_str());
+        }
+      } else{
+        Serial.printf("[HTTPS] FAILED: %s\n", https.errorToString(status).c_str());
+      }
+      https.end();
+    } else{
+      Serial.println("[HTTPS] Unable to connect");
+    }
+  }
 }
 
 void loop() {
-
-  display.println("Hello from VS Code");
-  if(WiFi.status() == WL_CONNECTED){
-    int status = 0;
-    WiFiClient client;
-    HTTPClient http;
-
-    http.begin(client, "https://graphql.anilist.co");
-    http.addHeader("Content-Type", "application/json");
-
-    String userQuery = "{User(search:\"barrettotte\"){id name statistics{anime{meanScore episodesWatched minutesWatched}}}}";
-    status = http.POST("{\"query\": \"" + userQuery + "\"}");
-    
-    if(status > 0){
-      const String& resp = http.getString();
-      Serial.printf("[HTTP] POST (%d)\n", status);
-
-      if(status == HTTP_CODE_OK){  
-        Serial.printf("[HTTP] SUCCESS:\n%s\n", resp.c_str());
-      } else if(status >= 400){
-        Serial.printf("[HTTP] FAILED:\n%s\n", resp.c_str());
-      }
-    } else{
-      Serial.printf("[HTTP] POST FAILED; ERR: %s\n", http.errorToString(status).c_str());
-    }
-    http.end();
-  }
-  delay(50000);
-  display.clearDisplay();
-}
-
-
-// setup serial monitor
-void initSerial(){
-  Serial.begin(115200);
-  
-  Serial.println();
-  Serial.println();
-  Serial.println();
-
-  for(uint8_t t = 4; t > 0; t--){
-    Serial.printf("[SETUP] WAIT %d...\n", t);
-    Serial.flush();
-    delay(1000);
-  }
+  //
 }
 
 
 // connect to wifi using SSID and password in include/config.h
 void initWifi(){
+  WiFi.mode(WIFI_STA);
   WiFi.begin(_WIFI_SSID, _WIFI_PWD);
-  Serial.print("Connecting..");
+
+  dualPrint("Connecting..");
   while(WiFi.status() != WL_CONNECTED){
     delay(500);
+    //dualPrint(".");
     Serial.print(".");
   }
-  Serial.printf("\nConnected to %s.\n", _WIFI_SSID.c_str());
+  //dualPrint("SSID: " + _WIFI_SSID + "\n\rIP: " + WiFi.localIP().toString(), true);
+  Serial.println("SSID: " + _WIFI_SSID + "\nIP: " + WiFi.localIP().toString());
 }
 
 
 // setup OLED display and text properties
 void initDisplay(){
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)){
-    Serial.println(F("[DSPLY] SSD1306 allocation failed"));
+    Serial.println(F("SSD1306 allocation failed"));
     for(;;);
   }
-  delay(2000);
-  display.clearDisplay();
-
+  delay(1500);
+  clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
+}
+
+
+// reset OLED
+void clearDisplay(){
+  display.clearDisplay();
   display.setCursor(0, 0);
+  delay(500);
+}
+
+
+// print to serial and OLED in one go
+void dualPrint(const String line){
+  dualPrint(line, false);
+}
+
+void dualPrint(const String line, bool clr){
+  if(clr){
+    clearDisplay();
+  }
+  Serial.print(line);
+  display.print(line);
   display.display();
 }
